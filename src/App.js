c6eca3f259008bafd5027cc40d04d53e55472172
Gurveer <gopal@ph.iitr.ac.in>
2022-01-22 11:35:15 +0530

    adding parallelx effects and formatting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import './App.css';
-import Navbar from './components/navbar';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './components/Home/home';
+import "./App.css";
+import Navbar from "./components/navbar";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/home";
 
 function App() {
+  // Parallel Scroll components effects
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  // -------------------
+
   return (
     <>
       <Router>
-        <Navbar/>
+        <Navbar />
         <Routes>
-        {/* Mapping multiple path for one component */}
-          {['/', '/techsellence_reactjs','/Home'].map((path) => (
+          {/* Mapping multiple path for one component */}
+          {["/", "/techsellence_reactjs", "/Home"].map((path) => (
             <Route path={path} element={<Home />} />
           ))}
         </Routes>
--- src/components/Carditem.js
+++ src/components/Carditem.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Cards";
 import "./Cards.css";
 
 function CardItems(props) {
+  // Parallel Scroll components effects
+  const [offsetY, setOffsetY] = useState(0);
+  const handleScroll = () => setOffsetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  // -------------------
+
   return (
     //   <>
     //       <li className="cards__item">
@@ -21,24 +33,27 @@ function CardItems(props) {
     //       </li>
     //   </>
 
-
     <>
-      <li className='cards__item'>
-        <Link className='cards__item__link' to={props.path}>
-          <figure className='cards__item__pic-wrap' data-category={props.label}>
+      <li className="cards__item">
+        <Link
+          className="cards__item__link"
+          style={{ transform: `translateY(-${offsetY * 0.1}px)` }}
+          to={props.path}
+        >
+          <figure className="cards__item__pic-wrap" data-category={props.label}>
             <img
-              className='cards__item__img'
-              alt='TravelImage'
+              className="cards__item__img"
+              alt="TravelImage"
               src={props.src}
             />
           </figure>
-          <div className='cards__item__info'>
-            <h5 className='cards__item__text'>{props.text}</h5>
+          <div className="cards__item__info">
+            <h5 className="cards__item__text">{props.text}</h5>
           </div>
         </Link>
       </li>
     </>
-  )
+  );
 }
 
 export default CardItems;
